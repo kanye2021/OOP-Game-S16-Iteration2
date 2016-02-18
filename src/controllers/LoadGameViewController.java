a54dac4a78dd58b1bdd076d200c52fca7ee7d288
David <dyeung93@hotmail.com>
2016-02-18 12:00:18 -0500

    added file retrive functionality back to controller instead of view

--- src/controllers/LoadGameViewController.java
+++ src/controllers/LoadGameViewController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import utilities.IOUtilities;
 import utilities.State;
 import utilities.StateManager;
 import views.LoadGameView;
@@ -14,14 +15,17 @@ import java.util.ArrayList;
  * Created by dyeung on 2/18/16.
  */
 public class LoadGameViewController extends ViewController {
-    //Variables
+    //Constants
+    private final String SAVE_FILE_LOCATION = IOUtilities.getFileSystemDependentPath("./src/res/save_files/");
+    //Variables for game view
     private int myOption;
-
     private ArrayList<File> listOfSaveFiles; //This is only being retrieved from the LoadGameView
 
     public LoadGameViewController(View view, StateManager stateManager){
         super(view, stateManager);
         myOption = 0;
+        listOfSaveFiles = new ArrayList<>();
+        updateSaveFiles(); // Initial update for save files
     }
     public void handleKeyPress(KeyEvent e){
 
@@ -50,26 +54,50 @@ public class LoadGameViewController extends ViewController {
         sendViewUpdatedOption();
         stateManager.refreshState();
     }
+    public void handleKeyRelease(KeyEvent e){
 
-    //TODO: Insert good name for these helper functions
+    }
+
+/*-------------------Main functions --------------*/
     public void updateSaveFiles(){
-        listOfSaveFiles = ((LoadGameView) view).getListOfSaveFiles();
+        File folder = new File(SAVE_FILE_LOCATION);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        File[] f = folder.listFiles();
+        //TODO: good idea? limited the amount of save files in case there are too many to show on screen
+        int limit = 5;
+        listOfSaveFiles.clear(); // Needs to reset the array whenever it retrieves the new set of files
+        for (int i = 0; i < f.length && i < limit; i++) {
+            File current = f[i];
+            // Checks if its a valid name (IE not .DS_Store) and adds it to the array list
+            if (checkValidFileName(current.getName()))  {
+                listOfSaveFiles.add(current);
+            }
+        }
+        ((LoadGameView) view).updateSaveFiles(listOfSaveFiles); //Sends the list of save files to the view
+    }
+
+    //Added this because the check valid part might be longer in the future
+    public boolean checkValidFileName(String name) {
+        //TODO: Add more checks (IE what about .bd.sf.xml or a.xml.df.sf)
+        if (name.contains(".xml") && !name.equals(".xml")){
+            return true;
+        }
+        return false;
     }
     public void sendViewUpdatedOption(){
         ((LoadGameView) view).updateOption(myOption);
     }
+
     public void loadGame(){ // Loads the game based on the current option
         System.out.println("Load game!");
-        //TODO: Add game view
+        //TODO: Remove this stuff and add game view
         TestView testView = new TestView(view.getScreenWidth(), view.getScreenHeight());
         TestViewController testController = new TestViewController(testView, stateManager);
         State nextState = new State(testController, testView);
         stateManager.setActiveState(nextState);
-
     }
 
-    public void handleKeyRelease(KeyEvent e){
-
-    }
 
 }
--- src/views/LoadGameView.java
+++ src/views/LoadGameView.java
@@ -1,42 +1,16 @@
 package views;
 
-import controllers.LoadGameViewController;
-import controllers.StartMenuViewController;
-import utilities.IOUtilities;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by dyeung on 2/18/16.
  */
-/*
-  //----------View Design stuff --------
-    private final int BUTTON_WIDTH = 200;
-    private final int BUTTON_HEIGHT = 50;
-    private final int START_POSITION = 100;
-    //--------File Path stuff -------
-    private String saveFilePath ="src/res/save_files/";
-    private File[] listOfSaveFiles;
-    public LoadGameView(){
-        saveFilePath = saveFilePath.replaceAll("\\\\|/", "\\"+System.getProperty("file.separator"));
-        this.viewController = new LoadGameController(this);
-//        File folder = new File(saveFilePath);
-        listOfSaveFiles = ((LoadGameController)viewController).getFileNames();
-        //getNewFiles();
-    }
-    public void getNewFiles(){ //Function is used to update the list of save files in the folder
-        listOfSaveFiles = ((LoadGameController)this.viewController).loadNewFolder();
-        System.out.println("LGV: " + listOfSaveFiles.length);
-    }
- */
-public class LoadGameView extends View {
-    //Constants
-    private final String SAVE_FILE_LOCATION = IOUtilities.getFileSystemDependentPath("./src/res/save_files/");
 
+public class LoadGameView extends View {
     //Scalable variables for resizing
     private int buttonWidth;
     private int buttonHeight;
@@ -50,11 +24,9 @@ public class LoadGameView extends View {
 
     public LoadGameView (int width, int height){
         super(width, height);
-        listOfSaveFiles = new ArrayList<>();
         selectedOption = 0;
     }
     public void render(Graphics g){
-        retrieveSaveFiles();
         if (listOfSaveFiles.isEmpty()) {
             renderTitle(g,false); //Changes the message of the title to no save files present
         }else {
@@ -89,74 +61,43 @@ public class LoadGameView extends View {
         int START_FILES = g.getFontMetrics(titleFont).getHeight() + titleButtonMargin;
         FontMetrics fm = g.getFontMetrics(buttonFont);
         g.setFont(buttonFont);
-        //display list of files
+        Color primaryColor;
+        Color secondaryColor;
+        //Displays all the buttons for the files found
         for (int i = 0; i < listOfSaveFiles.size(); i++) {
             File file = listOfSaveFiles.get(i);
-            // Exclude .DS_Store file lol.
-            if (file.isFile() && !file.getName().equals(".DS_Store")) {
-                String fileName = file.getName();
-                Rectangle2D rectangle = fm.getStringBounds(fileName, g);
+            String fileName = file.getName();
+            Rectangle2D rectangle = fm.getStringBounds(fileName, g);
 
-                int boxX = getScreenWidth() / 2 - buttonWidth / 2;
-                int boxY = buttonHeight * i + START_FILES;
-                int boxDX = buttonWidth;
-                int boxDY = buttonHeight;
+            int boxX = getScreenWidth() / 2 - buttonWidth / 2;
+            int boxY = buttonHeight * i + START_FILES;
+            int boxDX = buttonWidth;
+            int boxDY = buttonHeight;
+            int stringX = getScreenWidth() / 2 - (int) (rectangle.getWidth() / 2);
+            int stringY = i * buttonHeight + (int) (rectangle.getHeight() / 2) + fm.getAscent() + START_FILES;
 
-                int stringX = getScreenWidth() / 2 - (int) (rectangle.getWidth() / 2);
-                int stringY = i * buttonHeight + (int) (rectangle.getHeight() / 2) + fm.getAscent() + START_FILES;
+            if (i == selectedOption) {
+                primaryColor = Color.WHITE;
+                secondaryColor = Color.BLACK;
 
-                Color primaryColor;
-                Color secondaryColor;
+            } else {
+                primaryColor = Color.BLACK;
+                secondaryColor = Color.WHITE;
 
-                if (i == selectedOption) {
-                    primaryColor = Color.WHITE;
-                    secondaryColor = Color.BLACK;
-
-                } else {
-                    primaryColor = Color.BLACK;
-                    secondaryColor = Color.WHITE;
-
-                }
-                g.setColor(primaryColor);
-                g.fillRect(boxX, boxY, boxDX, boxDY);
-                g.setColor(secondaryColor);
-                g.drawString(fileName, stringX, stringY);
             }
+            g.setColor(primaryColor);
+            g.fillRect(boxX, boxY, boxDX, boxDY);
+            g.setColor(secondaryColor);
+            g.drawString(fileName, stringX, stringY);
         }
+
     }
-    //Function is used to look into the save file folder and retrieve the list of proper save files
-    public void retrieveSaveFiles(){
-        File folder = new File(SAVE_FILE_LOCATION);
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-        File[] f = folder.listFiles();
-        //TODO: Should there be a limit to the amount of save files?
-        //TODO: Might cause a problem with screen if the there are too many save files
-        int limit = 5;
-        listOfSaveFiles.clear(); // Needs to reset the array whenever it retrieves the new set of files
-        for (int i = 0; i < f.length && i <= limit; i++) {
-            File current = f[i];
-            // Checks if its a valid name (IE not .DS_Store) and adds it to the array list
-            if (checkValidFileName(current.getName()))  {
-                listOfSaveFiles.add(current);
-            }
-        }
+    //Called by the view controller
+    public void updateSaveFiles(ArrayList<File> newList){
+        listOfSaveFiles = newList;
     }
     public void updateOption(int newOption){
         selectedOption = newOption;
     }
-    //Helper functions
-    public boolean checkValidFileName(String name) {
-        //TODO: Simple file name but might need to be upgraded for future use (IE what about .bd.sf.xml or a.xml.df.sf)
-        //file name has to contain the .xml extension but can not be just ".xml"
-        if (name.contains(".xml") && !name.equals(".xml")){
-            return true;
-        }
-        return false;
-    }
-    //Accessor
-    public ArrayList<File> getListOfSaveFiles(){
-        return listOfSaveFiles;
-    }
+
 }
