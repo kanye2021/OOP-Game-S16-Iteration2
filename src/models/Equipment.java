357bdb3366d10d23cf53a6b7f07be997f3eb6867
DEPC <pegdavid@gmail.com>
2016-02-24 16:10:59 -0500

    Implemented Equipment

--- src/models/Equipment.java
+++ src/models/Equipment.java
@@ -1,5 +1,7 @@
 package models;
 
+import models.items.takeable.TakeableItem;
+
 /**
  * Created by aseber on 2/21/16.
  */
@@ -17,5 +19,81 @@ public class Equipment {
         TWO_HANDED_WEAPON;
 
     }
+
+    //Represents equipped items
+    private TakeableItem helmet;
+    private TakeableItem chestplate;
+    private TakeableItem greave;
+    private TakeableItem boots;
+    private TakeableItem gloves;
+    private TakeableItem shield;
+    private TakeableItem oneHandedWeapon;
+    private TakeableItem twoHandedWeapon;
+
+    //Getter methods
+    public TakeableItem getHelmet() {
+        return helmet;
+    }
+
+    public TakeableItem getChestplate() {
+        return chestplate;
+    }
+
+    public TakeableItem getGreave() {
+        return greave;
+    }
+
+    public TakeableItem getBoots() {
+        return boots;
+    }
+    public TakeableItem getGloves() {
+        return gloves;
+    }
+
+    public TakeableItem getShield() {
+        return shield;
+    }
+
+    public TakeableItem getOneHandedWeapon() {
+        return oneHandedWeapon;
+    }
+
+    public TakeableItem getTwoHandedWeapon() {
+        return twoHandedWeapon;
+    }
+
+    //Setter Methods
+    public void setHelmet(TakeableItem helmet) {
+        this.helmet = helmet;
+    }
+
+    public void setChestplate(TakeableItem chestplate) {
+        this.chestplate = chestplate;
+    }
+
+    public void setGreave(TakeableItem greave) {
+        this.greave = greave;
+    }
+
+    public void setBoots(TakeableItem boots) {
+        this.boots = boots;
+    }
+
+    public void setGloves(TakeableItem gloves) {
+        this.gloves = gloves;
+    }
+
+    public void setShield(TakeableItem shield) {
+        this.shield = shield;
+    }
+
+    public void setOneHandedWeapon(TakeableItem oneHandedWeapon) {
+        this.oneHandedWeapon = oneHandedWeapon;
+    }
+
+    public void setTwoHandedWeapon(TakeableItem twoHandedWeapon) {
+        this.twoHandedWeapon = twoHandedWeapon;
+    }
+
 
 }
