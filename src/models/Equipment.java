873a29b367fa00b76ab5cd46cdb5059c1cf3c73c
DEPC <pegdavid@gmail.com>
2016-02-24 16:24:38 -0500

    Implemented the equipment class variables and geters/setters

--- src/models/Equipment.java
+++ src/models/Equipment.java
@@ -1,5 +1,7 @@
 package models;
 
+import models.items.takeable.TakeableItem;
+
 /**
  * Created by aseber on 2/21/16.
  */
@@ -18,4 +20,76 @@ public class Equipment {
 
     }
 
+    private TakeableItem helmet;
+    private TakeableItem chestplate;
+    private TakeableItem greaves;
+    private TakeableItem boots;
+    private TakeableItem gloves;
+    private TakeableItem shield;
+    private TakeableItem oneHandedWeapon;
+    private TakeableItem twoHandedWeapon;
+
+    public void setHelmet(TakeableItem helmet) {
+        this.helmet = helmet;
+    }
+
+    public void setChestplate(TakeableItem chestplate) {
+        this.chestplate = chestplate;
+    }
+
+    public void setGreaves(TakeableItem greaves) {
+        this.greaves = greaves;
+    }
+
+    public void setBoots(TakeableItem boots) {
+        this.boots = boots;
+    }
+
+    public void setGloves(TakeableItem gloves) {
+        this.gloves = gloves;
+    }
+
+    public void setShield(TakeableItem shield) {
+        this.shield = shield;
+    }
+
+    public void setOneHandedWeapon(TakeableItem oneHandedWeapon) {
+        this.oneHandedWeapon = oneHandedWeapon;
+    }
+
+    public void setTwoHandedWeapon(TakeableItem twoHandedWeapon) {
+        this.twoHandedWeapon = twoHandedWeapon;
+    }
+
+    public TakeableItem getHelmet() {
+        return helmet;
+    }
+
+    public TakeableItem getChestplate() {
+        return chestplate;
+    }
+
+    public TakeableItem getGreaves() {
+        return greaves;
+    }
+
+    public TakeableItem getBoots() {
+        return boots;
+    }
+
+    public TakeableItem getGloves() {
+        return gloves;
+    }
+
+    public TakeableItem getShield() {
+        return shield;
+    }
+
+    public TakeableItem getOneHandedWeapon() {
+        return oneHandedWeapon;
+    }
+
+    public TakeableItem getTwoHandedWeapon() {
+        return twoHandedWeapon;
+    }
 }
