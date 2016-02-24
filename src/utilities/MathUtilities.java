3eb5d7f064274f0b352431b45d7bd05d81af3740
aseber <aseber@techsouth.cc>
2016-02-24 16:34:15 -0500

    Added MathUtilities stuff. Needed for stats

---
+++ src/utilities/MathUtilities.java
@@ -0,0 +1,14 @@
+package utilities;
+
+/**
+ * Created by aseber on 2/24/16.
+ */
+public class MathUtilities {
+
+    public static int putInRange(int min, int current, int max) {
+
+        return Math.min(Math.max(min, current), max); /// This function returns  a number between [min, max] given a current number
+
+    }
+
+}
