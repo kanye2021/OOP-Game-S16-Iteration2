17d967dab7873f18aef89bafb788fca221f348ba
Sergio Puleri <spuleri@ufl.edu>
2016-03-01 10:43:03 -0500

    Substates and Toast messages working

--- src/views/GameView.java
+++ src/views/GameView.java
@@ -3,8 +3,11 @@ package views;
 import models.entities.Avatar;
 import models.map.Map;
 import models.stats.Stats;
+import utilities.SubState;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 /**
  * Created by Bradley on 2/26/2016.
@@ -13,9 +16,11 @@ public class GameView extends View {
 
     private AreaViewport areaViewport;
     private StatusViewport statusViewport;
+    private ArrayList<SubState> substates;
 
     public GameView(int width, int height, Display display){
         super(width, height, display);
+        this.substates = new ArrayList<SubState>();
 
     }
 
@@ -33,6 +38,10 @@ public class GameView extends View {
             areaViewport.render(g);
             statusViewport.render(g);
         }
+        // Render all subviews on top of the AreaViewPort.
+        for (SubState subview : this.substates) {
+            subview.render(g);
+        }
     }
 
     @Override
@@ -45,5 +54,35 @@ public class GameView extends View {
     @Override
     public void scaleView() {
 
+    }
+
+    public void addSubState(SubState s) {
+        s.setParent(this);
+        this.substates.add(s);
+    }
+    public void insertSubState(SubState s, int index) {
+        s.setParent(this);
+        this.substates.add(index, s);
+    }
+    public ArrayList<SubState> getSubStates() {
+        return this.substates;
+    }
+    public boolean passInputToSubstate(KeyEvent e) {
+        // If the game view has substates, pass the input to the top-most substate.
+        if (!substates.isEmpty()) {
+            SubState top = substates.get(substates.size() - 1);
+            if (top.getViewController() != null) {
+                top.handleInput(e);
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+    public void removeSubState(SubState substate) {
+        substates.remove(substate);
+    }
+    public void clearSubStates() {
+        substates.clear();
     }
 }
