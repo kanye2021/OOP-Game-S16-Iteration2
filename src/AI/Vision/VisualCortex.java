65cd229524beeb5bf88bd8e30ebdf1ea75d13e6b
aseber <aseber@techsouth.cc>
2016-03-13 21:16:20 -0400

    Added TileUtilities

--- src/AI/Vision/VisualCortex.java
+++ src/AI/Vision/VisualCortex.java
@@ -5,8 +5,12 @@ import models.entities.Entity;
 import models.entities.npc.NPC;
 import models.items.Item;
 import models.map.Map;
+import models.map.Tile;
+import utilities.TileUtilities;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by Bradley on 3/5/16.
@@ -28,91 +32,37 @@ public class VisualCortex {
     // For now, all visual info is processed 120 degrees in the direction of the orientation.
     public void process() {
 
-        Map.Direction orientation = npc.getOrientation();
-        Point startLocation = orientation.neighbor(npc.getLocation());
+        // Get all the tiles the entity can see
+        HashMap<Tile, Point> tiles = TileUtilities.getTilesRecursively(map, npc.getLocation(), npc.getRadiusOfVisiblility());
 
-        // Determine which directions the entity can see.
-        switch(orientation){
-            case NORTH:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
-            case NORTH_EAST:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
-            case SOUTH_EAST:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
-            case SOUTH:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
-            case SOUTH_WEST:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
-            case NORTH_WEST:
-                processRecursively(Map.Direction.NORTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.NORTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_EAST, npc.getRadiusOfVisiblility(), startLocation);
-                processRecursively(Map.Direction.SOUTH_WEST, npc.getRadiusOfVisiblility(), startLocation);
-                break;
+        Tile tile;
+        Point pointOfTile;
+
+        Entity entityAtTile;
+        Item itemAtTile;
+        for (java.util.AbstractMap.Entry<Tile, Point> pair : tiles.entrySet()) {
+
+            tile = pair.getKey();
+            pointOfTile = pair.getValue();
+
+            entityAtTile = tile.getEntity();
+
+            if (entityAtTile != null) {
+
+                memory.addVisualInput(entityAtTile, pointOfTile);
+
+            }
+
+            itemAtTile = tile.getItem();
+
+            if (itemAtTile != null) {
+
+                memory.addVisualInput(itemAtTile, pointOfTile);
+
+            }
+
         }
 
-    }
-
-    private void processRecursively(Map.Direction orientation, int visualRange, Point currentLocation){
-        cycles++;
-        if(visualRange == 0){
-            return;
-        }
-
-        if (!map.isTileValid(currentLocation)) {
-            return;
-        }
-
-        // Get entities here.
-        Entity entity = map.getEntityAt(currentLocation);
-        if(entity!=null){
-            memory.addVisualInput(entity, currentLocation);
-            cycles = 0;
-        }
-
-        // Get items here.
-        Item item = map.getItemAt(currentLocation);
-        if(item!=null){
-            memory.addVisualInput(item, currentLocation);
-        }
-
-        // update the currentLocation and visual range;
-        visualRange--;
-        currentLocation = orientation.neighbor(currentLocation);
-
-        processRecursively(orientation, visualRange, currentLocation);
     }
 
 }
---
+++ src/utilities/TileUtilities.java
@@ -0,0 +1,101 @@
+package utilities;
+
+import models.map.Map;
+import models.map.Tile;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by aseber on 3/13/16.
+ */
+public class TileUtilities {
+
+    // This function intentionally ignores the center point
+    public static HashMap<Tile, Point> getTilesRecursively(Map map, Point center, int radius) {
+
+//        function cube_spiral(center, radius):
+//        var results = [center]
+//        for each 1 ≤ k ≤ radius:
+//        results = results + cube_ring(center, k)
+//        return results
+
+        HashMap<Tile, Point> tiles = new HashMap<>();
+
+        for (int i = 1; i < radius; ++i) {
+
+            tiles.putAll(getTileRing(map, center, i));
+
+        }
+
+        return tiles;
+
+    }
+
+    // This function intentionally ignores the center tile
+    public static HashMap<Tile, Point> getTileRing(Map map, Point center, int radius) {
+
+        HashMap<Tile, Point> tiles = new HashMap<>();
+        Point point = new Point(center);
+        Tile tileToAdd;
+
+        if (radius <= 1) {
+
+            return tiles;
+
+        }
+
+        for (int i = 0; i < radius; ++i) {
+
+            point = Map.Direction.NORTH.neighbor(point);
+
+        }
+
+        for (Map.Direction direction : Map.Direction.values()) {
+
+
+            for (int i = 0; i < radius; ++i) {
+
+                tileToAdd = map.getTileAt(point);
+
+                if (tileToAdd != null) {
+
+                    tiles.put(tileToAdd, point);
+
+                }
+
+                point = direction.neighbor(point);
+
+            }
+
+        }
+
+        return tiles;
+
+    }
+
+    // This function takes in a set of tiles and applies a mask to them where
+    // inputTiles refers to the tiles that the function takes in.
+    // center refers to the center point, can use the same one you used to get the tiles originally
+    // direction is [0, 2pi] and refers to the direction angle.
+    // span is [0, pi] and refers to the angle to add to each side of direction to add
+    private static HashSet<Tile> maskTilesByAngle(HashSet<Tile> inputTiles, Point center, double direciton, double span) {
+
+        MathUtilities.putInRange(0.0, direciton, 2 * Math.PI);
+        MathUtilities.putInRange(0.0, span, Math.PI);
+
+        return inputTiles;
+
+    }
+
+    private static double getAngle(Point startPoint, Point endPoint) {
+
+        double x = endPoint.x - startPoint.x;
+        double y = endPoint.y - startPoint.y;
+
+        return Math.asin(y / x);
+
+    }
+
+}
